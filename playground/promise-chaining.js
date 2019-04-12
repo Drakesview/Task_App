4885cf70eefce9b0d2735ef8c745dd3d413770de
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2019-04-12 17:42:45 +0100

    =second commit

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,12 +3,24 @@ const User = require('../src/models/user')
 
 //5ca4993dd62c6734c877def8
 
-User.findByIdAndUpdate('5ca49a222279a31200dea490', { age: 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({age: 1})
-}).then((count) => {
+// User.findByIdAndUpdate('5ca49a222279a31200dea490', { age: 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age: 1})
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({age})
+
+    return count
+}
+
+updateAgeAndCount('5ca49a222279a31200dea490', 2).then((count)=>{
     console.log(count)
 }).catch((e) => {
     console.log(e)
 })
-
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5c9e38751d0e0847284f1ed3').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed:false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5c9e38751d0e0847284f1ed3').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed:false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed:false})
+    return count
+}
+
+deleteTaskAndCount('5cab1a64940722381485d04d').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
